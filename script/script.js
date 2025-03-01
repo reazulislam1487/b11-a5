224ea3a1c657dcd599a9b83f023ed3e88210328a
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-03-01 20:57:43 +0600

    Create Script

--- script/script.js
+++ script/script.js
@@ -14,7 +14,34 @@ const allButton = document.getElementsByClassName("all-btn");
 for (i = 0; i < allButton.length; i++) {
   const button = allButton[i];
   button.addEventListener("click", function (event) {
-    
-  })
-    const color = event.target.id.replace("-color", "");
+    const click = event.target.innerHTML;
+
+    // Plus && Minus
+    if (click) {
+      const minus = parseInt(document.getElementById("minus").innerText);
+      const plus = parseInt(document.getElementById("plus").innerText);
+      const plusAdd = plus + 1;
+      const minusAdd = minus - 1;
+      document.getElementById("plus").innerText = plusAdd;
+      document.getElementById("minus").innerText = minusAdd;
+      this.disabled = true;
+      this.style.backgroundColor = "rgba(55, 82, 253, 0.21)";
+      alert("Board updated Successfully");
+      //   create history
+      const history = document.getElementById("history");
+      const p = document.createElement("p");
+      p.innerHTML = `
+                     <p class="bg-[#F4F7FF] p-3 text-gray-500 mt-0 my-3  rounded-xl text-sm">You have Complete The Task
+                        Add
+                        Dark Mode at 12:48:15 PM
+                    </p>              
+      `;
+      history.appendChild(p);
+      //   remove history
+      document.getElementById("clear").addEventListener("click", function () {
+        history.innerHTML = "";
+      });
+    }
+    // alert("okay");
+  });
 }
