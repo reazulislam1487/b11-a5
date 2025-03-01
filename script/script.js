f5103302659187a03bbb8f8b74df6d780abc48ff
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-03-02 02:58:11 +0600

    fixed some issues

--- script/script.js
+++ script/script.js
@@ -5,15 +5,20 @@ document.getElementById("button").addEventListener("click", function () {
 });
 // current date
 
-// time
+// date set
 let date = new Date();
 
 document.getElementById("date").innerHTML = date.toDateString();
+// time set
+const time = new Date();
+date.setHours(12);
+date.setMinutes(48);
+date.setSeconds(15);
+let currentTime = time.toLocaleTimeString();
 
 // main function
 
 const allButton = document.getElementsByClassName("all-btn");
-// console.log(allButton);
 for (i = 0; i < allButton.length; i++) {
   const button = allButton[i];
   button.addEventListener("click", function (event) {
@@ -23,30 +28,36 @@ for (i = 0; i < allButton.length; i++) {
     let title = child.textContent;
 
     // Plus && Minus
-    if (click) {
-      const minus = parseInt(document.getElementById("minus").innerText);
-      const plus = parseInt(document.getElementById("plus").innerText);
-      const plusAdd = plus + 1;
-      const minusAdd = minus - 1;
-      document.getElementById("plus").innerText = plusAdd;
-      document.getElementById("minus").innerText = minusAdd;
-      this.disabled = true;
-      this.style.backgroundColor = "rgba(55, 82, 253, 0.21)";
+
+    const minus = parseInt(document.getElementById("minus").innerText);
+    const plus = parseInt(document.getElementById("plus").innerText);
+    const plusAdd = plus + 1;
+    const minusAdd = minus - 1;
+    document.getElementById("plus").innerText = plusAdd;
+    document.getElementById("minus").innerText = minusAdd;
+    this.disabled = true;
+    this.style.backgroundColor = "rgba(55, 82, 253, 0.21)";
+
+    if (minusAdd === 0) {
       alert("Board updated Successfully");
-      //   create history
-      const history = document.getElementById("history");
-      const p = document.createElement("p");
-      p.innerHTML = `
+      alert("Congrates!!! You have completed all the current task");
+    } else {
+      alert("Board updated Successfully");
+    }
+
+    //   create history
+    const history = document.getElementById("history");
+    const p = document.createElement("p");
+    p.innerHTML = `
                      <p class="bg-[#F4F7FF] p-3 text-gray-500 mt-0 my-3  rounded-xl text-sm">You have Complete The Task
-                        ${title} at 12:48:15 PM
+                        ${title} at ${currentTime}.
                     </p>              
       `;
-      history.appendChild(p);
-      //   remove history
-      document.getElementById("clear").addEventListener("click", function () {
-        history.innerHTML = "";
-      });
-    }
+    history.appendChild(p);
+    //   remove history
+    document.getElementById("clear").addEventListener("click", function () {
+      history.innerHTML = "";
+    });
   });
 }
 
